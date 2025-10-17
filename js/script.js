415e2e5be91ec26d2323cb7a880eb8813a2d16d7
Mateus Vilaça <mateusdasilvavilaca@gmail.com>
2025-10-16 21:06:24 -0300

    Adicionando novos campos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const form = document.querySelector('#formCarp');
 const cadastro = document.querySelector('.botao');
 
+const proxPagina = () => {
+    form.style.display = 'none';
+}
+
 const addLoading = () => {
     cadastro.innerHTML = '<img class="loding" src="img/loding.png" alt="">';
 };
@@ -14,9 +18,13 @@ const handleSubmit = (event) => {
     event.preventDefault();
     addLoading();
 
+
+    const caracteristica = document.querySelector('#icaracteristica').value;
+    const medicao = document.querySelector('#imetMedicao').value;
     const codigo = document.querySelector('#icodigo').value;
     const produto = document.querySelector('#iproduto').value;
-    const diametro = document.querySelector('#idiametro').value;
+    const minimo = document.querySelector('#imin').value;
+    const maximo = document.querySelector('#imax').value;
 
     fetch('https://sheetdb.io/api/v1/diu9gggbuf1l2', {
 
@@ -25,14 +33,12 @@ const handleSubmit = (event) => {
             'Accept': 'aplication/json',
             'Content-Type': 'aplication/json',
         },
-        body: JSON.stringify({ codigo, produto, diametro })
+        body: JSON.stringify({ produto, codigo, caracteristica, medicao, maximo, minimo })
 
     }).then(() => removeLoading());
     event.target.reset();
 };
 
-document.getElementById('formCap').addEventListener('submit', handleSubmit);
-
-form.reset();
+document.getElementById('formCap').addEventListener('submit', handleSubmit,);
 
 
